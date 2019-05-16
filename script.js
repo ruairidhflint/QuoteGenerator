eca647ae611b087b125bbb8376731dc4f84384c3
Rory Flint <rory@roryflint.co.uk>
2019-05-16 19:05:41 +0100

    styling and revelation

--- script.js
+++ script.js
@@ -3,18 +3,19 @@ const numberGenerator = Math.round(Math.random() * (quoteData.length - 1));
 
 
 function giveMeAQuote (){
-  return `${quoteData[numberGenerator].quote} <br> - <br> ${quoteData[numberGenerator].name}`
+  return `${quoteData[numberGenerator].quote}`
 }
 
 
 
-let text = document.querySelector('.text-box');
+let text = document.querySelector('.quote');
+let name = document.querySelector('.author');
 
 text.innerHTML = giveMeAQuote ();
+name.innerHTML = quoteData[numberGenerator].name;
 
-
-let button = document.querySelector('button')
+let button = document.querySelector('.button');
+let button2 = document.querySelector('.wrapper1');
 
 button.addEventListener('click', () => window.location.reload());
-
-
+button2. addEventListener('click', () => name.classList.toggle('hidden'))
