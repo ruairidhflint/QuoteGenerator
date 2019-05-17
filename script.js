bb1d5a87a95ba60ed0087d37381bd727ba8a4d61
Rory Flint <rory@roryflint.co.uk>
2019-05-17 10:53:53 +0100

    updated button to not reload whole page.

--- script.js
+++ script.js
@@ -1,22 +1,12 @@
-
-const numberGenerator = Math.round(Math.random() * (quoteData.length - 1));
-
-
-function giveMeAQuote (){
-  return `${quoteData[numberGenerator].quote}`
-}
-
-
-
 let text = document.querySelector('.quote');
 let name = document.querySelector('.author');
-
-text.innerHTML = giveMeAQuote();
-name.innerHTML = quoteData[numberGenerator].name;
-
 let button = document.querySelector('.button');
-// let button2 = document.querySelector('.wrapper1');
 
-// button.addEventListener('click', () => window.location.reload());
-button.addEventListener('click', () => window.location.reload());
-button2. addEventListener('click', () => name.classList.toggle('hidden'))
+button.addEventListener('click', quoteGenerator );
+
+function quoteGenerator(){
+  const numberGenerator = Math.round(Math.random() * (quoteData.length - 1));
+  text.innerHTML = quoteData[numberGenerator].quote
+  name.innerHTML = quoteData[numberGenerator].name;
+}
+
